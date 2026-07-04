03375a72ed29e3ebdbd5f08e08863b8a4702cab6
cubocr25 <cubocr25@gmail.com>
2026-07-04 11:37:27 -0600

    EmpleadoTest actualizado
    se crea el test de empleado basado en test estudiante

--- src/test/java/edu/uam/educore/model/personas/EmpleadoTest.java
+++ src/test/java/edu/uam/educore/model/personas/EmpleadoTest.java
@@ -4,10 +4,67 @@
  */
 package edu.uam.educore.model.personas;
 
-/**
- *
- * @author Cubo
- */
-public class EmpleadoTest {
-    
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.uam.educore.enums.TipoPersonal;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+class EmpleadoTest {
+
+    @Test
+    void constructor_crea_empleado_correctamente() {
+
+        Empleado e = new Empleado(
+                1,
+                "Daniel",
+                "Cubero",
+                "cubero@test.pro",
+                123456,
+                LocalDate.of(2025, 1, 15),
+                TipoPersonal.TECNICO);
+
+        assertEquals(1, e.getId());
+        assertEquals("Daniel", e.getNombre());
+        assertEquals("Cubero", e.getApellidos());
+        assertEquals("cubero@test.pro", e.getEmail());
+        assertEquals(123456, e.getSalario(), 0.01);
+        assertEquals(LocalDate.of(2025, 1, 15), e.getFechaIngreso());
+        assertEquals(TipoPersonal.TECNICO, e.getTipoPersonal());
+    }
+
+    @Test
+    void getTipo_retorna_texto_correcto() {
+
+        Empleado e = new Empleado(
+                1,
+                "Carolina",
+                "Granados",
+                "granados@test.pro",
+                654321,
+                LocalDate.now(),
+                TipoPersonal.GUARDA);
+
+        assertEquals("GUARDA", e.getTipo());
+    }
+
+    @Test
+    void getInfo_incluye_nombre_tipo_y_salario() {
+
+        Empleado e = new Empleado(
+                1,
+                "Roko",
+                "Loco",
+                "Loco@test.pro",
+                987654,
+                LocalDate.of(2025, 1, 15),
+                TipoPersonal.CONSERJE);
+
+        String info = e.getInfo();
+
+        assertTrue(info.contains("Roko"));
+        assertTrue(info.contains("CONSERJE"));
+        assertTrue(info.contains("987654"));
+    }
+
 }
